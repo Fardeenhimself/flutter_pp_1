1f5ff7ca498bf0e18502fd70703e8af91ecf69df
fardeenhimself <fardeenviper77@gmail.com>
2025-05-31 00:35:27 +0600

    Angela Yu PP 1

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.green[200],
+        body: SafeArea(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(color: Colors.red, width: 100.0),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    color: Colors.yellow,
+                    height: 100.0,
+                    width: 100.0,
+                    margin: EdgeInsets.symmetric(horizontal: 55.0),
+                  ),
+                  Container(color: Colors.green, height: 100.0, width: 100.0),
+                ],
+              ),
+              Container(color: Colors.blue, width: 100.0),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
