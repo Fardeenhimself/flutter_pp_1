f963e598a02564a7a9ea54ea5db1dc89b73cc70b
fardeenhimself <fardeenviper77@gmail.com>
2025-05-31 16:11:58 +0600

    Flutter Business Card

--- lib/main.dart
+++ lib/main.dart
@@ -11,25 +11,86 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.green[200],
+        appBar: AppBar(
+          title: Text('Business Card'),
+          titleTextStyle: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.green[400],
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(color: Colors.red, width: 100.0),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    color: Colors.yellow,
-                    height: 100.0,
-                    width: 100.0,
-                    margin: EdgeInsets.symmetric(horizontal: 55.0),
-                  ),
-                  Container(color: Colors.green, height: 100.0, width: 100.0),
-                ],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/bhai.png'),
+                radius: 70.0,
               ),
-              Container(color: Colors.blue, width: 100.0),
+              Text.rich(
+                TextSpan(
+                  text: 'Md Azimusshan ',
+                  style: TextStyle(fontSize: 40.0, fontFamily: 'Cookie'),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: 'Rohomot',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Cookie',
+                        fontSize: 40.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Text(
+                'DAYI & ACTIVIST',
+                style: TextStyle(
+                  fontFamily: 'SFPR',
+                  fontSize: 20.0,
+                  color: Colors.green.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+                width: 100.0,
+                child: Divider(color: Colors.green.shade200),
+              ),
+              Card(
+                color: Colors.green[500],
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                child: ListTile(
+                  leading: Icon(Icons.phone, color: Colors.green.shade900),
+                  title: Text(
+                    '+88 017 1234 5678',
+                    style: TextStyle(
+                      fontFamily: 'SFPR',
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  //trailing: Icon(Icons.radio_button_checked),
+                ),
+              ),
+              Card(
+                color: Colors.green[500],
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                child: ListTile(
+                  leading: Icon(Icons.email, color: Colors.green.shade900),
+                  title: Text(
+                    'md.rohomot@eorsaj.com',
+                    style: TextStyle(
+                      fontFamily: 'SFPR',
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
